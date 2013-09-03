6404b3493bb4336b5bdaa181f363e51bdedd769f
yaarenas <yamid.arenas@teamdetroit.com>
2013-09-03 12:44:15 -0300

    test of equality

---
+++ src/com/tdiba/yaarenas/equality.java
@@ -0,0 +1,15 @@
+package com.tdiba.yaarenas;
+
+public class equality{
+	
+	public static void myConstructor(){
+		
+		int control;
+		control = 100;
+		boolean hayError = control == 100;
+		
+		System.out.println(hayError);
+		
+	} 
+	
+}
